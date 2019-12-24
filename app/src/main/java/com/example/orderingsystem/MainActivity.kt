06f8e5b7c1b594a044d92a7e3eadd72ed3f948cc
Greg840717 <asdorm97531@gmail.com>
2019-12-24 16:36:42 +0800

    finish mainActivity layout and MenuActivity layout.

--- app/src/main/java/com/example/orderingsystem/MainActivity.kt
+++ app/src/main/java/com/example/orderingsystem/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.orderingsystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun GoToMenu(v : View){
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
     }
 }
