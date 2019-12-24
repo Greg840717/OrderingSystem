6b76531b719c5756c057948063f531153c2aa3c5
Greg840717 <asdorm97531@gmail.com>
2019-12-24 18:39:17 +0800

    complete the menuActivity result back to MainActivity.

--- app/src/main/java/com/example/orderingsystem/MainActivity.kt
+++ app/src/main/java/com/example/orderingsystem/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.example.orderingsystem
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        data?.extras?.let {
+            if(requestCode==1 && resultCode == Activity.RESULT_OK){
+                order_drink.text = it.get("Drink").toString()
+                order_sweet.text = it.get("Sweet").toString()
+                order_ice.text = it.get("Ice").toString()
+            }
+        }
     }
 
     fun GoToMenu(v : View){
         val intent = Intent(this, MenuActivity::class.java)
-        startActivity(intent)
+        startActivityForResult(intent, 1)
     }
 }
--- app/src/main/java/com/example/orderingsystem/MenuActivity.kt
+++ app/src/main/java/com/example/orderingsystem/MenuActivity.kt
@@ -1,12 +1,32 @@
 package com.example.orderingsystem
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        sentMenu.setOnClickListener {
+            if(drink_name.length()<2){
+                Toast.makeText(this,getString(R.string.drink_name),Toast.LENGTH_LONG).show()
+            }
+            else{
+                var bundle = Bundle()
+                bundle.putString("Drink",drink_name.text.toString())
+                bundle.putString("Sweet",radioGroup.findViewById<RadioButton>(radioGroup.checkedRadioButtonId).text.toString())
+                bundle.putString("Ice",radioGroup2.findViewById<RadioButton>(radioGroup2.checkedRadioButtonId).text.toString())
+                setResult(Activity.RESULT_OK, Intent().putExtras(bundle))
+                finish()
+            }
+        }
     }
 }
